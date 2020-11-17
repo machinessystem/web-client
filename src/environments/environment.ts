6d1d1fe02c1eb9d022fbc67fef61b846f84ecd95
Bhawani Shankar Bharti <bhawanishiv@gmail.com>
2020-11-18 03:24:49 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,16 +5,7 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:4000/api',
-  firebaseConfig: {
-    apiKey: "AIzaSyBwKGh1MmoJ0dhRe_f23CkVO-dyEz1qMHU",
-    authDomain: "machinesproject.firebaseapp.com",
-    databaseURL: "https://machinesproject.firebaseio.com",
-    projectId: "machinesproject",
-    storageBucket: "machinesproject.appspot.com",
-    messagingSenderId: "271717380110",
-    appId: "1:271717380110:web:aaaeffa2b3cc17ea338c5f",
-    measurementId: "G-SP3F6ZSYZE"
-  },
+  firebaseConfig: "firebase-web-config-object"
 };
 
 /*
